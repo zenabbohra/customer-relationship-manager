8e74a4420b3aebd7c93a54c55c122b73403cdcc2
Zenab Bohra <zenab.bohra19@gmail.com>
2021-03-15 15:17:03 +1100

    fixup! Initial commit

---
+++ src/main/java/zenab/project/customerRelationshipManager/CustomerRelationshipManagerApplication.java
@@ -0,0 +1,13 @@
+package zenab.project.customerRelationshipManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRelationshipManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRelationshipManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zenab/project/customerRelationshipManager/CustomerRelationshipManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package zenab.project.customerRelationshipManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRelationshipManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
